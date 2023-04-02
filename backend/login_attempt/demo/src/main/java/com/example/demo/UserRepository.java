99796c7d47bfe316021788ea392f3bae67c0b99c
dand <dandodun@gmail.com>
2023-04-02 20:45:51 +0300

    add login

---
+++ backend/login_attempt/demo/src/main/java/com/example/demo/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+    User findByUsername(String username);
+}
