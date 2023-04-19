0cc96d9f827f1ade92c6c07261f27bb2752e4dbb
Iulian-Tudor <itscutaru@gmail.com>
2023-04-19 10:31:40 +0300

    Made the 3 methods. Needs to be linked with Database through Spring Data (works with docker, I checked) to be able to create and put or delete from that AdRepo ads.

--- backend/Facebook/src/main/java/org/Facebook/service/AdCompanyService.java
+++ backend/Facebook/src/main/java/org/Facebook/service/AdCompanyService.java
@@ -1,17 +1,28 @@
 package org.Facebook.service;
 
 import org.Facebook.model.entity.Ad;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdCompanyService {
+  @Autowired
+  private AdRepository adRepository; // presupunem ca AdRepo e un Spring Data Repository, legatura cu docker database
+
   public Ad createAd(String title, String content) {
-  return null;
+    Ad ad = new Ad();
+    ad.setTitle(title);
+    ad.setContent(content);
+    ad.setNumberOfClicks(0);
+    return adRepository.save(ad);
   }
+
   public void deleteAd(Ad ad) {
+    adRepository.delete(ad);
   }
+
   public int checkStats(Ad ad) {
-  return 0;
+    return ad.getNumberOfClicks();
   }
 
 }
