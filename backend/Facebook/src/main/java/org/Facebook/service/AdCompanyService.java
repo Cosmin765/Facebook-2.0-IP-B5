08c911b0550ff29944ac4811553f8aed28542166
Iulian-Tudor <itscutaru@gmail.com>
2023-04-20 13:33:04 +0300

    Restructured add service. Still needs a proper query in Ad interface.

--- backend/Facebook/src/main/java/org/Facebook/service/AdCompanyService.java
+++ backend/Facebook/src/main/java/org/Facebook/service/AdCompanyService.java
@@ -6,23 +6,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AdCompanyService {
-  /*@Autowired
-  private AdRepository adRepository; // presupunem ca AdRepo e un Spring Data Repository, legatura cu docker database
 
   public Ad createAd(String title, String content) {
-    Ad ad = new Ad();
-    ad.setTitle(title);
-    ad.setContent(content);
-    ad.setNumberOfClicks(0);
-    return adRepository.save(ad);
+    return null;
   }
 
   public void deleteAd(Ad ad) {
-    adRepository.delete(ad);
-  } */
+  }
 
   public int checkStats(Ad ad) {
-    return ad.getNumberOfClicks();
+    return 0;
   }
 
 }
