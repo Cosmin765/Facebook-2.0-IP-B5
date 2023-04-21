2fd9646b6941555df692514c85bcca7c287a8e7d
Turtureanu Cosmin <cosminturtureanu1234@gmail.com>
2023-04-21 15:51:26 +0300

    Add user grapg

--- backend/Facebook/src/main/java/org/Facebook/controller/UserController.java
+++ backend/Facebook/src/main/java/org/Facebook/controller/UserController.java
@@ -1,14 +1,18 @@
 package org.Facebook.controller;
 
+import org.Facebook.mapper.UserMapper;
 import org.Facebook.model.entity.User;
 import org.Facebook.model.dto.UserDto;
 import org.Facebook.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+//@RestController
+@Controller
 public class UserController {
     @Autowired
     private UserService userService;
@@ -17,11 +21,17 @@ public class UserController {
         userService.registerUser(user);
     }
     @GetMapping(value = "/users")
+    @ResponseBody
     public List<UserDto> getUsers(){
         return userService.getUsers();
     }
-
+    @GetMapping(value = "/friends")
+    @ResponseBody
+    public List<UserDto> getFriends(@RequestParam int userId) {
+        return userService.getFriends(userId).stream().map(UserMapper::toDto).toList();
+    }
     @GetMapping(value = "/")
+    @ResponseBody
     public String getWelcome(){
         return "Hello world!";
     }
--- backend/Facebook/src/main/java/org/Facebook/service/UserService.java
+++ backend/Facebook/src/main/java/org/Facebook/service/UserService.java
@@ -1,9 +1,11 @@
 package org.Facebook.service;
 
-import org.Facebook.adapter.AppSecurityConfig;
+import org.Facebook.config.AppSecurityConfig;
 import org.Facebook.mapper.UserMapper;
 import org.Facebook.model.dto.UserDto;
+import org.Facebook.model.entity.Friendship;
 import org.Facebook.model.entity.User;
+import org.Facebook.repository.FriendshipRepository;
 import org.Facebook.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,6 +18,8 @@ import java.util.List;
 public class UserService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private FriendshipRepository friendshipRepository;
     public void registerUser(User user) {
         String hashedPassword = AppSecurityConfig.getPasswordEncoder().encode(user.getPassword());
         user.setPassword(hashedPassword);
@@ -23,6 +27,14 @@ public class UserService implements UserDetailsService {
     }
     public List<UserDto> getUsers() {
         return userRepository.findAll().stream().map(UserMapper::toDto).toList();
+    }
+
+    public List<User> getFriends(int userId) {
+        List<Friendship> friendships = friendshipRepository.getFriends(userId);
+
+        return friendships.stream()
+                .map(friendship -> friendship.getUser1().getId() == userId ? friendship.getUser2() : friendship.getUser1())
+                .toList();
     }
 
     @Override
