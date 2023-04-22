e03684742be526f3eb703f73c29c37b733abb63b
radu-alex-012 <radualex02@yahoo.com>
2023-04-22 14:21:43 +0300

    Update AdCompanyController.java

--- backend/Facebook/src/main/java/org/Facebook/controller/AdCompanyController.java
+++ backend/Facebook/src/main/java/org/Facebook/controller/AdCompanyController.java
@@ -8,24 +8,31 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class AdCompanyController {
     @Autowired
     private AdCompanyService adCompanyService;
 
-    @PostMapping(value="/create_ad")
+    @PostMapping(value = "/create_ad")
+    @ResponseBody
     public void createAd(@RequestParam String title) {
         adCompanyService.createAd(title);
     }
 
-    @PostMapping(value="/delete_ad")
+    @PostMapping(value = "/delete_ad")
+    @ResponseBody
     public void deleteAd(@RequestParam(name = "id") int adId) {
         adCompanyService.deleteAd(adId);
     }
 
-    @GetMapping(value="/check_ad_stats")
+    @GetMapping(value = "/check_ad_stats")
     @ResponseBody
-    public String checkAdStats(@RequestParam(name = "id") int adId) {
-        return adCompanyService.checkStats(adId).toString() + " clicks";
+    public Map<String, Integer> checkAdStats(@RequestParam(name = "id") int adId) {
+        Map<String, Integer> json = new HashMap<>();
+        json.put("clicks", adCompanyService.checkStats(adId));
+        return json;
     }
 }
