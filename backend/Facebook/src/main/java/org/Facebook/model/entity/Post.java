8b24fe9f3ee4cefe08aa1d0e8392db53b6b4b4c9
Turtureanu Cosmin <cosminturtureanu1234@gmail.com>
2023-04-25 21:14:24 +0300

    Stash

--- backend/Facebook/src/main/java/org/Facebook/model/entity/Post.java
+++ backend/Facebook/src/main/java/org/Facebook/model/entity/Post.java
@@ -15,7 +15,6 @@ public class Post {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Id
   private String id;
-  private String title;
   private String content;
   @ManyToOne
   @JoinColumn(name = "user_id")
