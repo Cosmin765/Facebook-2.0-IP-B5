0ea896f47416549a6f25316c3c459119e993f2cb
Hrebenciuc Alexandru-Ioan <100340563+Hrebe@users.noreply.github.com>
2023-04-30 19:00:27 +0300

    Update-Profile

--- FRONTEND/frontfp/src/components/Profile.js
+++ FRONTEND/frontfp/src/components/Profile.js
@@ -1,22 +1,22 @@
 import React from "react";
 import "./Profile.css"
 import "./Feed.css"
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheckSquare, faCoffee  } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import StretchedMenu from './stretched_menu.js';
 import likeBtn from './icons/like.svg'
 import commentBtn from './icons/comment.svg'
 import shareBtn from './icons/share.svg'
 import arrow from './icons/arrow.svg'
+import icon5 from './icons/search.svg';
+import icon6 from './icons/notif.svg';
+import icon7 from './icons/out.svg';
 
-library.add(faCheckSquare, faCoffee)
 
 const posts = [
     {account:{name:'Dwayne Johnson', picture: require('./img/dwayne-johnson.jpg'), uploadDate:'02.01.2023'}, text:'Made my historic rap debut (thankfully I didn’t suck😅) Huge shout to all the hip hop & music fans for your HYPE reactions that are straight f*cking fire 🔥🔥🔥🙏🏾👊🏾', picture:null, video:'https://www.youtube.com/embed/E9T78bT26sk'},
-    {account:{name:'Kevin Hart', picture: require('./img/kevin-hart.jpg'), uploadDate:'30.02.2023'}, text:'A lot of blood, sweat, and tears have gone into this career of mine.', picture:require('./img/kevin-hart-feed.jpg'), video:null},
-    {account:{name:'Andrew Tate', picture: require('./img/andrew-tate.jpg'), uploadDate:'21.01.2023'}, text:'The Matrix may have imprisoned me, But I am free inside The Real World.', picture:require('./img/free-tate.jpg'), video:null},
-    {account:{name:'Hugh Jackman', picture:require('./img/hugh-jackman.jpg'), uploadDate:'24.03.2023'}, text:'@ryanReynolds is my best friend :3', picture:null, video:null},
-    {account:{name:'Jeremy Clarkson', picture: require('./img/jeremy-clarkson.jpg'), uploadDate:'14.03.2023'}, text:'I’m not homophobic, I enjoy watching lesbians on the internet.', picture:require('./img/jk.jpg'), video:null}
+    {account:{name:'Dwayne Johnson', picture: require('./img/dwayne-johnson.jpg'), uploadDate:'30.02.2023'}, text:'A lot of blood, sweat, and tears have gone into this career of mine.', picture:require('./img/kevin-hart-feed.jpg'), video:null},
+    {account:{name:'Dwayne Johnson', picture: require('./img/dwayne-johnson.jpg'), uploadDate:'21.01.2023'}, text:'The Matrix may have imprisoned me, But I am free inside The Real World.', picture:require('./img/free-tate.jpg'), video:null},
+    {account:{name:'Dwayne Johnson', picture:require('./img/dwayne-johnson.jpg'), uploadDate:'24.03.2023'}, text:'@ryanReynolds is my best friend :3', picture:null, video:null},
+    {account:{name:'Dwayne Johnson', picture: require('./img/dwayne-johnson.jpg'), uploadDate:'14.03.2023'}, text:'I’m not homophobic, I enjoy watching lesbians on the internet.', picture:require('./img/jk.jpg'), video:null}
   ];
  
   function Feed(){
@@ -94,29 +94,46 @@ const posts = [
 
 const Profile = () => {
     return(
-        <div className='container'>
-            <div className="left">koobecaf.</div>
-            <div className="right">
-                <div className="search-bar">
-                    <form className="search">
-                        <input type="text" placeholder="Search.." name="search"></input>
-                        <button type="submit"><FontAwesomeIcon icon="check-square" /></button> 
-                     </form>
-                </div>
-                <div className="content">
-                    <div className="upper">
-                        .
+        <div className='container-profile'>
+           <div className="left-profile">
+           <StretchedMenu />
+           </div>
+            <div className="right-profile">
+            <div className="top-bar">
+              <div className="search-profile">
+                  <div className="search_box-profile">
+                    <img src={icon5}></img>
+
+                  </div>
+                  <div className="right_icons-profile">
+                    <img src={icon6}></img>
+                    <img src={icon7}></img>
+                  </div>
+              </div>
+             </div> 
+               
+                <div className="content-profile">
+                    <div className="upper-profile">
+                        <div className="colored-cover">
+                            <div className="profile-photo">
+                            <img src={'./img/dwayne-johnson.jpg'} alt={"PROFILE PICTURE"} /> 
+                            </div>  
+                          </div>
+                        <div className="white-cover"></div>
                     </div>
-                    <div className="bottom">
-                        <div className="left-content">
-                            .
+                    <div className="bottom-profile">
+                        <div className="left-content-profile">
+                            <p>Data nasterii: 22.09.1999</p>
+                            <p>Necasatorit(a)</p>
+                            <p>A absolvit Colegiul National din Iasi</p>
                         </div>
-                        <div className="right-content">
+                        <div className="right-content-profile">
                             <Feed />
                         </div>
                     </div>
                 </div>
-            </div>
+           
+             </div>
         </div>
     )
 }
