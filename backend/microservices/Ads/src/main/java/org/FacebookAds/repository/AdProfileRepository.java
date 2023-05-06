4d8aab3fc4d658f23b499675808c4e3eede3331a
iustinnn <98700219+iustinnn@users.noreply.github.com>
2023-05-06 13:16:34 +0300

    updated repos
    new queries

--- backend/microservices/Ads/src/main/java/org/FacebookAds/repository/AdProfileRepository.java
+++ backend/microservices/Ads/src/main/java/org/FacebookAds/repository/AdProfileRepository.java
@@ -6,9 +6,20 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AdProfileRepository extends JpaRepository<AdProfile, Integer> {
     @Query(value = "SELECT * FROM ad_profiles where user_id = :user_id", nativeQuery = true)
     AdProfile findByUserId(@Param("user_id") Integer userId);
+
+
+   @Query(value = "SELECT id, user_id, keyword_id FROM ad_profiles WHERE user_id = :user_id", nativeQuery = true)
+    List<AdProfile> getAllProfilesById(@Param("user_id") Integer userId);
+
+   // new id
+    @Query(value = "SELECT id FROM ad_profiles WHERE user_id = :user_id", nativeQuery = true)
+    Integer getAdProfileIdByUserId(@Param("user_id") Integer userId);
+
 
 }
