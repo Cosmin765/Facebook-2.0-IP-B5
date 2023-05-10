ce832d691ae1c38115c267f923cb3fbc5e6baee0
Hrebenciuc Alexandru-Ioan <100340563+Hrebe@users.noreply.github.com>
2023-05-10 16:35:53 +0300

    Update Profile&MyProfile

--- FRONTEND/frontfp/src/components/Popup.js
+++ FRONTEND/frontfp/src/components/Popup.js
@@ -1,27 +1,35 @@
+// Popup.js
 import React, { useState } from "react";
 
-import './Popup.css'
-function Popup(props) {
-    const [description, setDescription] = useState(props.descriptionText);
-  
-    const handleSave = () => {
-      props.setDescriptionText(description);
-      props.setTrigger(false);
-    };
-  
-    return props.trigger ? (
-      <div className="profile_popup">
-        <div className="profile_popup-inner">
-          <button className="profile_close-btn" onClick={() => props.setTrigger(false)}>
-            close
-          </button>
-          <h2>Edit your description</h2>
-          <textarea className="profile_setDescription" value={description} onChange={(e) => setDescription(e.target.value)} />
-          <br></br>
-          <button className="profile_save-btn" onClick={handleSave}>Save</button>
-        </div>
+import './Popup.css';
+
+function Popup({ trigger, setTrigger, descriptionText, setDescriptionText }) {
+  const [description, setDescription] = useState(descriptionText);
+
+  const handleSave = () => {
+    setDescriptionText(description);
+    setTrigger(false);
+  };
+
+  return trigger ? (
+    <div className="profile_popup">
+      <div className="profile_popup-inner">
+        <button className="profile_close-btn" onClick={() => setTrigger(false)}>
+          Close
+        </button>
+        <h2>Edit your description</h2>
+        <textarea
+          className="profile_setDescription"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <br />
+        <button className="profile_save-btn" onClick={handleSave}>
+          Save
+        </button>
       </div>
-    ) : null;
-  }
-  
-export default Popup
+    </div>
+  ) : null;
+}
+
+export default Popup;
