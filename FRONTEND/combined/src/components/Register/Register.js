fb0b00f128e189c0c317741a2c99f54152cb5e09
Teodora Cohman <tcohman@yahoo.ro>
2023-05-13 16:11:33 +0300

    'register_date'

--- FRONTEND/combined/src/components/Register/Register.js
+++ FRONTEND/combined/src/components/Register/Register.js
@@ -30,45 +30,7 @@ const RegisterAccount = () => {
         <input className="register_input" type="text" placeholder="First Name" required/>
         <input  className="register_input" type="text" placeholder="Surname" required/>
         <div className="reg_birthday"> Birthday: </div>
-        <div className="reg_dropdown-container">
-          <select className="reg_dropdown" value={day} onChange={handleDayChange}>
-            <option value="Day" disabled>
-              Day
-            </option>
-            {[...Array(31)].map((_, index) => (
-              <option key={index + 1} value={index + 1}>
-                {index + 1}
-              </option>
-            ))}
-          </select>
-          <select className="reg_dropdown" value={month} onChange={handleMonthChange}>
-            <option value="Month" disabled>
-              Month
-            </option>
-            <option value="January">January</option>
-            <option value="February">February</option>
-            <option value="March">March</option>
-            <option value="April">April</option>
-            <option value="May">May</option>
-            <option value="June">June</option>
-            <option value="July">July</option>
-            <option value="August">August</option>
-            <option value="September">September</option>
-            <option value="October">October</option>
-            <option value="November">November</option>
-            <option value="December">December</option>
-          </select>
-          <select className="reg_dropdown2" value={year} onChange={handleYearChange}>
-            <option value="Year" disabled>
-              Year
-            </option>
-            {[...Array(100)].map((_, index) => (
-              <option key={index + 1920} value={index + 1920}>
-                {index + 1920}
-              </option>
-            ))}
-          </select>
-        </div>
+       <input className="register_input register_input_birth" type="date" placeholder="Birthday" required></input>
         <input className="register_input"  type="email" placeholder="Email" required/>
         <input  className="register_input" type="password" placeholder="Password" required/>
         <input  className="register_input" type="password" placeholder="Confirm Password" required/>
