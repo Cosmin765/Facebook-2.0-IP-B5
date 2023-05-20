cac20182be3e0fdf5faed2ab9e431accd65e8566
Teodora Cohman <tcohman@yahoo.ro>
2023-05-20 11:30:25 +0300

    search functional

--- FRONTEND/friends/src/App.js
+++ FRONTEND/friends/src/App.js
@@ -6,11 +6,24 @@ import icon7 from './icons/out.svg';
 //import LargeMenu from './large_menu';
 import MainContainer from './main_container'
 import StretchedMenu from './stretched_menu';
-import React from 'react';
+import React,{useState} from 'react';
 import MainContainer_Friend_Request from './main_container_friend_request';
 import Sugestions_Friends from './Sugestions_Friends';
 
 function App() {
+  const [searchText, setSearchText] = useState('');
+  const [filteredFriends, setFilteredFriends] = useState([]);
+
+  const friends = ['Alice', 'Bob', 'Carol', 'David', 'Eve'];
+  const handleSearch = (event) => {
+    const { value } = event.target;
+    setSearchText(value);
+
+    const filtered = friends.filter((friend) =>
+      friend.toLowerCase().includes(value.toLowerCase())
+    );
+    setFilteredFriends(filtered);
+  };
   return (
     <div className="container">
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat" />
@@ -21,9 +34,17 @@ function App() {
       
       <div className="container2">
         <div className="search">
-          <div className="search_box">
+          <div className={searchText !== ''?"search_box_activ":"search_box"}>
+          
             <img src={icon5}></img>
-
+            <input type="text" className="search_box_input" value={searchText} onChange={handleSearch} placeholder="Search friends"/>
+            {searchText !== '' && (
+            <ul className="friend-list">
+        {filteredFriends.map((friend, index) => (
+          <li key={index} className={index === 0 ? 'first-friend' : ''}>{friend}</li>
+        ))}
+      </ul> 
+  )}
           </div>
           <div className="right_icons">
             <img src={icon6}></img>
